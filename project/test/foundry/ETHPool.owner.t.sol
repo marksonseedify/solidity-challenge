0553457ffe15f031bf879046ea7a684ca6433b40
Théo RICHARD <mail@rcode.tech>
2023-01-12 18:26:10 +0100

    refactor: `ETHPoolTest` only test `owner` with set up file
    `ETHPoolTestSetUp`

--- project/test/foundry/ETHPool.owner.t.sol
+++ project/test/foundry/ETHPool.owner.t.sol
@@ -1,23 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import {ETHPoolTestSetUp} from "./setUp/ETHPoolTestSetUp.t.sol";
 
-import {ETHPool} from "../../src/ETHPool.sol";
-
-contract ETHPoolTest is Test {
-    address public constant FORGE = 0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84;
-
-    ETHPool public pool;
-
-    function setUp() public {
-        pool = new ETHPool();
+contract ETHPoolTest_Owner is ETHPoolTestSetUp {
+    function setUp() public override {
+        super.setUp();
     }
 
     /*//////////////////////////////////////////////////////////////
                                  BASIC ATTRIBUTES
     //////////////////////////////////////////////////////////////*/
     function test_owner_isForgeDeployer() public {
-        assertTrue(pool.owner() == FORGE);
+        assertTrue(pool.owner() == users.FORGE());
     }
 }
