bde2635e321be219187324775e18eda6f0d79ce5
Théo RICHARD <mail@rcode.tech>
2023-01-12 18:30:17 +0100

    test: fixtures file for `Users` & `TeamMembers`

---
+++ project/test/fixtures/TeamMembers.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract TeamMembers {
+    address public constant TEAM_MEMBER_1 =
+        0xaEcD92aEC5BfBe2f5a02DB2DeE90733897360983;
+    address public constant TEAM_MEMBER_2 =
+        0x8C8D7C46219D9205f056f28fee5950aD564d7465;
+    address public constant TEAM_MEMBER_3 =
+        0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5;
+}
---
+++ project/test/fixtures/Users.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Users {
+    // FORGE == owner
+    address public constant FORGE = 0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84;
+
+    address public constant ALICE = 0x690B9A9E9aa1C9dB991C7721a92d351Db4FaC990;
+    address public constant BOB = 0xDEe2C8F3345104f6DD081657D180A9058Be7Ab05;
+    address public constant CHARLIE =
+        0x42361e5Fd2Dc3282984127c090335856e46eAbe3;
+}
