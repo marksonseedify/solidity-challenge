bc7d837d4e8e7443f21ff2ac1b8a90c1d20f61b7
Théo RICHARD <mail@rcode.tech>
2023-01-13 01:05:14 +0100

    feat: implement deposit for dApp users

--- project/src/ETHPool.sol
+++ project/src/ETHPool.sol
@@ -20,6 +20,12 @@ contract ETHPool is Ownable {
         uint256 lastTotal;
     }
 
+    struct UserDeposit {
+        uint256 lastDeposit;
+        uint256 lastestTime;
+        uint256 total;
+    }
+
     struct Withdrawals {
         uint256 amount;
         uint256 lastWithdrawlTime;
@@ -39,8 +45,9 @@ contract ETHPool is Ownable {
     /*//////////////////////////////////////////////////////////////
                             TRACK USER'S DATA
     //////////////////////////////////////////////////////////////*/
-    mapping(address => uint256) public usersDeposits;
+    mapping(address => UserDeposit) public usersDeposits;
     mapping(address => Withdrawals) public usersWithdrawals;
+    uint256 public totalUsersDeposits;
 
     /*//////////////////////////////////////////////////////////////
                             EVENTS
@@ -53,6 +60,7 @@ contract ETHPool is Ownable {
         uint256 timestamp,
         address indexed user
     );
+    event UsersDeposit(address indexed user, uint256 indexed amount);
     event Withdrawl(
         address indexed user,
         uint256 indexed amount,
@@ -114,6 +122,27 @@ contract ETHPool is Ownable {
         ++nextWeek;
 
         emit RewardsDeposit(msg.value, block.timestamp, msg.sender);
+    }
+
+    /**
+     * @notice Users deposit ETH into the pool to earn rewards.
+     * @dev For calculations simplicity, we authorize users to deposit only
+     *      once a week.
+     */
+    function userDeposit() external payable {
+        require(msg.value > 0, "DEPOSIT_ZERO");
+        require(
+            block.timestamp - usersDeposits[msg.sender].lastestTime >= 1 weeks,
+            "DEPOSIT_ONCE_WEEK"
+        );
+
+        usersDeposits[msg.sender].lastDeposit = msg.value;
+        usersDeposits[msg.sender].lastestTime = block.timestamp;
+        usersDeposits[msg.sender].total += msg.value;
+
+        totalUsersDeposits += msg.value;
+
+        emit UsersDeposit(msg.sender, msg.value);
     }
 
     /**
